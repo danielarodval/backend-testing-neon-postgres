4d3dbdc34d7a1f84f122b1cbe30207167c6541a7
danielarodval <49764316+danielarodval@users.noreply.github.com>
2026-02-15 18:32:23 -0500

    Move update comment and tidy formatting
    non-functional cleanup ig

--- neon-solo-test/update_data.py
+++ neon-solo-test/update_data.py
@@ -3,18 +3,21 @@ import psycopg
 from dotenv import load_dotenv
 
 load_dotenv()
+
 conn_string = os.getenv("DATABASE_URL")
 
 try:
     with psycopg.connect(conn_string) as conn:
         print("Connected to the database successfully!")
+        
+        # update the data in the table
         with conn.cursor() as cur:
-            # update the data in the table
             cur.execute(
                 "UPDATE test_table SET name = 'Updated Name' WHERE id = 1"
             )
 
             print("Updated the record in test_table successfully.")
+            
 except Exception as e:
     print(f"An error occurred: {e}")
 finally:
